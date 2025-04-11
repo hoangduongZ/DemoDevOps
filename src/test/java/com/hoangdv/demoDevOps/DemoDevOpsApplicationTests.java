dfd84fe286b7f8db8001620aff8759321479bd51
HoangDuongViet <148684137+HoangDuongViet@users.noreply.github.com>
2025-04-11 12:25:35 +0700

    second

---
+++ src/test/java/com/hoangdv/demoDevOps/DemoDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangdv.demoDevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
